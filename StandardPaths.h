96c6d5072dc6637d787725701c9e3043b9f9717c
Rafael <Syping@users.noreply.github.com>
2016-03-28 13:27:45 +0200

    fixed StandardPaths commit lag

--- StandardPaths.h
+++ StandardPaths.h
@@ -15,14 +15,19 @@
 * You should have received a copy of the GNU General Public License
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 *****************************************************************************/
+
 #ifndef STANDARDPATHS_H
 #define STANDARDPATHS_H
 
-#include <QtGlobal>
-#if QT_VERSION >= 0x050000
-#include "StandardPaths5.h"
-#else
-#include "StandardPaths4.h"
-#endif
+#include <QString>
+
+class StandardPaths
+{
+public:
+    StandardPaths();
+    static QString dataLocation();
+    static QString desktopLocation();
+    static QString documentsLocation();
+};
 
 #endif // STANDARDPATHS_H
