a12edb32bb752d2257a036a95635ba0616b254a6
Rafael <Syping@users.noreply.github.com>
2016-04-14 06:57:04 +0200

    direct from str convert

--- StringParser.cpp
+++ StringParser.cpp
@@ -54,7 +54,7 @@ QString StringParser::convertLogStringForDraw(const QString &inputStr)
 QString StringParser::convertBuildedString(const QString &buildedStr)
 {
     QString outputStr = buildedStr;
-    outputStr.replace("$SHAREDIR", QString::fromUtf8(GTA5SYNC_SHARE));
+    outputStr.replace("$SHAREDIR", QString(GTA5SYNC_SHARE));
     outputStr.replace("$RUNDIR", QFileInfo(qApp->applicationFilePath()).absoluteDir().absolutePath());
     outputStr.replace("$SEPARATOR", QDir::separator());
     return outputStr;
