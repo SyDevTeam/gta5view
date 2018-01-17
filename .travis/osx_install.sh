67dfcf09ee87d944164b95a70a733eae4015e259
Syping <schiedelrafael@keppe.org>
2018-01-17 22:19:27 +0100

    added build scripts for OS X

---
+++ .travis/osx_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install packages
+brew install qt
--- .travis/travis.sh
+++ .travis/travis.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # Install lua
-sudo apt-get update -qq && \
-sudo apt-get install -qq lua5.2
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+	sudo apt-get update -qq && \
+	sudo apt-get install -qq lua5.2
+elif [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+	brew install lua
+fi
 
-if [ `git name-rev --tags --name-only $(git rev-parse HEAD)` == "undefined" ]; then export APPLICATION_VERSION=`lua5.2 -e 'for line in io.lines("config.h") do local m = string.match(line, "#define GTA5SYNC_APPVER \"(.+)\"$"); if m then print(m); os.exit(0) end end'`; else export APPLICATION_VERSION=`git name-rev --tags --name-only $(git rev-parse HEAD)`; fi
+if [ `git name-rev --tags --name-only $(git rev-parse HEAD)` == "undefined" ]; then export APPLICATION_VERSION=`lua -e 'for line in io.lines("config.h") do local m = string.match(line, "#define GTA5SYNC_APPVER \"(.+)\"$"); if m then print(m); os.exit(0) end end'`; else export APPLICATION_VERSION=`git name-rev --tags --name-only $(git rev-parse HEAD)`; fi
 export PACKAGE_VERSION=$(grep -oE '^[^\-]*' <<< $APPLICATION_VERSION)
 export PACKAGE_BUILD=$(grep -oP '\-\K.+' <<< $APPLICATION_VERSION)
 export EXECUTABLE_VERSION=${PACKAGE_VERSION}${PACKAGE_BUILD}
-if [ "$PACKAGE_BUILD" == "" ]; then export PACKAGE_BUILD=${TRAVIS_BUILD_NUMBER}; fi
+if [[ ! ${PACKAGE_BUILD} ]]; then export PACKAGE_BUILD=${TRAVIS_BUILD_NUMBER}; fi
 export PROJECT_DIR=$(pwd)
 
 .travis/${BUILD_SCRIPT}
