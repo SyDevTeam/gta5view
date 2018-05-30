6e335638a3dc062af6814040f0f133829e40a5b8
Syping <schiedelrafael@keppe.org>
2018-05-30 10:42:27 +0200

    fix dropbox

--- .travis/windows_travis.sh
+++ .travis/windows_travis.sh
@@ -8,6 +8,6 @@ cd ${PROJECT_DIR} && \
 docker pull ${DOCKER_IMAGE} && \
 docker run --rm \
 	-v "${PROJECT_DIR}:${PROJECT_DIR_DOCKER}" \
-	-v "~/.dropbox_uploader:/root/.dropbox_uploader" \
+	-v "${HOME}/.dropbox_uploader:/root/.dropbox_uploader" \
 	${DOCKER_IMAGE} \
 	/bin/bash -c "export PROJECT_DIR=${PROJECT_DIR_DOCKER} && export QT_SELECT=${QT_SELECT} && export APPLICATION_VERSION=${APPLICATION_VERSION} && export QMAKE_FLAGS_QT4=${QMAKE_FLAGS_QT4} && export QMAKE_FLAGS_QT5=${QMAKE_FLAGS_QT5} && export PACKAGE_VERSION=${PACKAGE_VERSION} && export PACKAGE_BUILD=${PACKAGE_BUILD} && export PACKAGE_CODE=${PACKAGE_CODE} && export EXECUTABLE_VERSION=${EXECUTABLE_VERSION} && export EXECUTABLE_ARCH=${EXECUTABLE_ARCH} && cd ${PROJECT_DIR_DOCKER} && .travis/windows_build.sh"
