d61cbd4743f090ef1331e301965d5bfd0f5ef5b7
Syping <schiedelrafael@keppe.org>
2018-06-22 19:00:36 +0200

    add Telemetry to GitLab build

--- .gitlab/gitlab.sh
+++ .gitlab/gitlab.sh
@@ -4,6 +4,10 @@
 apt-get update -qq && \
 apt-get install -qq curl git lua5.2 openssl
 
+# Decrypt Telemetry Authenticator
+rm -rf tmext/TelemetryClassAuthenticator.cpp && \
+openssl aes-256-cbc -k $tca_pass -in .gitlab/TelemetryClassAuthenticator.cpp.enc -out tmext/TelemetryClassAuthenticator.cpp -d
+
 # Check if build is not tagged
 if [ "${CI_COMMIT_TAG}" == "" ]; then
 	export EXECUTABLE_TAG=-$(git rev-parse --short HEAD)
