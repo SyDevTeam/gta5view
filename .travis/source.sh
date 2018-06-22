99f3c22ab062a6ced04e8da3bc5a8ff19d44e59f
Syping <schiedelrafael@keppe.org>
2018-06-22 14:59:39 +0200

    update telemetry blob

--- .travis/source.sh
+++ .travis/source.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf tmext/TelemetryClassAuthenticator.cpp && \
-openssl aes-256-cbc -K $encrypted_55502862a724_key -iv $encrypted_55502862a724_iv -in tmext/TelemetryClassAuthenticator.cpp.enc -out tmext/TelemetryClassAuthenticator.cpp -d && \
+openssl aes-256-cbc -K $encrypted_db000a5d87d6_key -iv $encrypted_db000a5d87d6_iv -in .travis/TelemetryClassAuthenticator.cpp.enc -out tmext/TelemetryClassAuthenticator.cpp -d && \
 openssl aes-256-cbc -K $encrypted_d57e7d2f8877_key -iv $encrypted_d57e7d2f8877_iv -in .travis/dropbox_uploader.enc -out ~/.dropbox_uploader -d
