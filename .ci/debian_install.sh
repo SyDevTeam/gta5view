7ba53226430256f5a931ec8c311e7253cfb052d7
Syping <schiedelrafael@keppe.org>
2018-11-18 20:39:32 +0100

    fix CI script SVG module installation

--- .ci/debian_install.sh
+++ .ci/debian_install.sh
@@ -2,4 +2,4 @@
 
 # Install packages
 apt-get update -qq && \
-apt-get install -qq ${APT_INSTALL} checkinstall dpkg-dev fakeroot g++ gcc qtbase5-dev qt5-qmake qttranslations5-l10n libqt4-dev
+apt-get install -qq ${APT_INSTALL} checkinstall dpkg-dev fakeroot g++ gcc qtbase5-dev qt5-qmake qttranslations5-l10n libqt4-dev libqt5svg5-dev
